cac77f55609bd0d6d560a018fdb2b14bc1f86843
victorio-kalelle <kalelleee@gmail.com>
2025-05-13 15:29:24 +0800

    add group TODOs for Exercise 11

---
+++ main.py
@@ -0,0 +1,31 @@
+# Group: Polytech
+
+#TODO (Victorio):
+#    Create a module named after your last name. (victorio.py)
+#    create a function that does anything
+#    Use at least 1 external module from PyPI
+
+#TODO (Raquem):
+#    Create a module named after your last name. (raquem.py)
+#    Create a function that does anything
+#    Use at least 1 external module from PyPI
+
+#TODO (Niones):
+#    Create a module named after your last name. (niones.py)
+#    Create a function that does anything
+#    Use at least 1 external module from PyPI
+
+
+#TODO (Capilitan):
+#    Create a module named after your last name. (capilitan.py)
+#    Create a function that does anything
+#    Use at least 1 external module from PyPI
+
+#TODO (Villarta):
+#    Create a module named after your last name. (villarta.py)
+#    Create a function that does anything
+#    Use at least 1 external module from PyPI
+
+#TODO (Everyone):
+#    Import all created modules in this file (main.py).
+#    Call the function from each module in this file.
