643eb747cbe3b5136a661a85672d814442fc2218
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-05-14 12:02:04 +0800

    add fake profile genarator

---
+++ polytechpackage/raquem.py
@@ -0,0 +1,25 @@
+from faker import Faker
+
+fake = Faker()
+
+def display_fake_profile():
+    """Generates and displays a fake user profile."""
+    print("\n---Generating Fake Profile---\n")
+
+    profile = {
+        "Name": fake.name(),
+        "Username": fake.user_name(),
+        "Date of Birth": fake.date_of_birth(minimum_age=18, maximum_age=30),
+        "Email": fake.email(),
+        "Address": fake.address(),
+        "Phone number": fake.phone_number(),
+        "Job": fake.job(),
+        "Website": fake.url(),
+        "Company": fake.company()
+    }
+
+    for key, value in profile.items():
+        print(f"{key}: {value}")
+
+
+
